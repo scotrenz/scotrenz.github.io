51c8e61ad1cb7dbda0f7f16ed92607bce2730aee
Scot Renz <scotrenz@gmail.com>
2018-02-15 11:41:31 -0500

    cards with projects

--- js/logic.js
+++ js/logic.js
@@ -1,49 +1,77 @@
-    function myMap() {
-        myCenter = new google.maps.LatLng(40.7831, -73.9712);
-        var mapOptions = {
-            center: myCenter,
-            zoom: 11,
-            scrollwheel: false,
-            draggable: false,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        var map = new google.maps.Map(document.getElementById("googleMap"), mapOptions);
+// import DirectionReveal from "direction-reveal";
 
-        var marker = new google.maps.Marker({
-            position: myCenter,
-        });
-        marker.setMap(map);
-    }
+// const directionRevealSwing = DirectionReveal({
+//     selector: '.direction-reveal',              // Container element selector.
+//     itemSelector: '.direction-reveal__card',    // Item element selector.
+//     animationName: 'swing',                     // Animation CSS class.
+//     enableTouch: true,                          // Adds touch event to show content on first click then follow link on the second click.
+//     touchThreshold: 250                         // Touch length must be less than this to trigger reveal which prevents the event triggering if user is scrolling.
+//   });
 
-    // Modal Image Gallery
-    function onClick(element) {
-        $("#img01").src = element.src;
-        $("#modal01").style.display = "block";
-        var captionText = document.getElementById("caption");
-        captionText.innerHTML = element.alt;
-    }
+var cards = document.querySelectorAll(".card"), i;
 
-    // Change style of navbar on scroll
-    // window.onscroll = function() { navbar() };
+for (i = 0; i < cards.length; i++) {
+  wiggle(cards[i])
+  
+};
 
-    // function navbar() {
-    //     var navbar = document.getElementById("myNavbar");
-    //     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-    //         navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " w3-black";
-    //     } else {
-    //         navbar.className = navbar.className.replace(" w3-card w3-animate-top w3-white", "");
-    //     }
-    // }
+function wiggle (card){
+  card.addEventListener('mousemove', function (e) {
+    var wh = window.innerHeight / 2,
+    ww = window.innerWidth / 2,
+    offX = this.offsetLeft,
+    offY = this.offsetTop
+    offW = this.offsetWidth/2;
+    this.style.setProperty('--mouseX', (e.clientX - offX - offW) / 25);
+    this.style.setProperty('--mouseY', (e.clientY - wh) / 25);
+  
+  });
 
-    // Used to toggle the menu on small screens when clicking on the menu button
-    function toggleFunction() {
-        var x = document.getElementById("mobileNav");
-        if (x.className.indexOf("w3-show") == -1) {
-            x.className += " w3-show";
-        } else {
-            x.className = x.className.replace(" w3-show", "");
-        }
-    }
+card.addEventListener('mouseleave', function (e) {
 
-    
- 
+  this.style.setProperty('--mouseX', 0);
+  this.style.setProperty('--mouseY', 0);
+
+});
+}
+
+
+
+// Modal Image Gallery
+function onClick(element) {
+  document.getElementById("img01").src = element.src;
+  document.getElementById("modal01").style.display = "block";
+  var captionText = document.getElementById("caption");
+  captionText.innerHTML = element.alt;
+}
+
+// Used to toggle the menu on small screens when clicking on the menu button
+function toggleFunction() {
+  var x = document.getElementById("mobileNav");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+
+//Google Map API
+function myMap() {
+    myCenter = new google.maps.LatLng(40.7831, -73.9712);
+    var mapOptions = {
+      center: myCenter,
+      zoom: 11,
+      scrollwheel: false,
+      draggable: false,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(
+      document.getElementById("googleMap"),
+      mapOptions
+    );
+  
+    var marker = new google.maps.Marker({
+      position: myCenter
+    });
+    marker.setMap(map);
+  }
